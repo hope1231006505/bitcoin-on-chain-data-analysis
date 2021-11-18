912e8ff64956acf01de6bae3ba8003e2fc93e069
hope <hope1231006505@gmail.com>
2021-11-18 07:41:00 -0600

    query bitcoin transaction data, outputs

---
+++ query/hodl_pull_outputs.sql
@@ -0,0 +1,12 @@
+SELECT
+  t.block_number
+  , t.hash as rcvd_tx_hash
+  , t.is_coinbase as is_coinbase
+  , o.index as rcvd_tx_id
+  , o.addresses[ORDINAL(1)] as address
+  , o.value
+FROM
+  `bigquery-public-data.crypto_bitcoin.transactions` t
+  , UNNEST(t.outputs) o
+WHERE 
+  block_timestamp_month = '{year}-{month}-01'
